fd5fbd0494eac95699ee6ced1946ababdb002f57
Erick Pires <pires@erickpires.com>
2016-06-17 23:55:42 -0300

    rebuilding site Sex Jun 17 23:55:42 BRT 2016

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+git subtree push --prefix=public git@github.com:spencerlyon2/hugo_gh_blog.git gh-pages
